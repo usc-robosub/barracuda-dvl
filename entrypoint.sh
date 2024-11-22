dbb2ed0f0812051f034c0ccbc9dfe47fb2f56dff
Mihir Singh <mihirsin@usc.edu>
2024-11-21 22:04:49 -0800

    modified restart line in compose file, exec line in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 source /opt/ros/noetic/setup.bash
 [ -f /opt/barracuda-dvl/catkin_ws/devel/setup.bash ] && source /opt/barracuda-dvl/catkin_ws/devel/setup.bash
-exec /bin/bash
+exec tail -f /dev/null
