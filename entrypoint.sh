fa1ea38e369bae7db27c38713644fe28b46b86ad
uscauv26 <uscauv@gmail.com>
2026-03-02 21:34:47 -0800

    updated ip address for dvl on ethernet

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 echo "source /opt/barracuda-dvl/catkin_ws/install/setup.bash" >> ~/.bashrc
 
 # ros2 launch waterlinked_dvl launch_dvl.launch.py
-ros2 run waterlinked_dvl barracuda-dvl-ros-driver.py --ros-args -p dvl_host:='192.168.194.95' -p client_address:='0.0.0.0'
+ros2 run waterlinked_dvl barracuda-dvl-ros-driver.py --ros-args -p dvl_host:='192.168.8.148' -p client_address:='0.0.0.0'
 # ros2 run dvl_a50 dvl_a50.py --ros-args -p dvl_address:='192.168.194.95' -p client_address:='0.0.0.0'
 # ros2 launch dvl_a50 dvl_a50.launch.py ip_address:='192.168.194.95'
